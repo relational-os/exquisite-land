520fab2cfc5daa0d97d6329f99f3fa76338bab68
CJ Pais <cj@cjpais.com>
2021-09-11 15:36:14 -0700

    add brush color on editor

--- packages/web/src/app/components/Editor.tsx
+++ packages/web/src/app/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import useStore, { TileStatus, TileType } from "../features/State";
 
@@ -13,6 +13,18 @@ const Editor = ({
 }) => {
   const pallete = useStore((state) => state.canvases[0].pallete);
   const [activeColor, setActiveColor] = useState("#000");
+  const [activeBrushSize, setActiveBrushSize] = useState(4);
+  const [activeTool, setActiveTool] = useState<"pen" | "eraser">("pen");
+
+  const circleSVG = `'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewbox="0 0 ${
+    activeBrushSize * 2
+  } ${
+    activeBrushSize * 2
+  }" width="${activeBrushSize}px" height="${activeBrushSize}px"><circle cx="${
+    activeBrushSize / 2
+  }px" cy="${activeBrushSize / 2}px" r="${
+    activeBrushSize / 2
+  }px" fill="${activeColor.replace("#", "%23")}"/></svg>'`;
 
   async function handleSave() {
     console.log(`tile(${x}, ${y}) saved!`);
@@ -39,31 +51,75 @@ const Editor = ({
     border: "0.0625rem solid #9c9c9c",
     borderRadius: "0.25rem",
   };
+
+  useEffect(() => {
+    console.log("setting active tool to", activeTool == "eraser");
+    canvasRef.current?.eraseMode(activeTool == "eraser");
+  }, [activeTool]);
+
   return (
     <div className="editor">
-      <ReactSketchCanvas
-        ref={canvasRef}
-        height="600px"
-        width="600px"
-        strokeWidth={4}
-        strokeColor={activeColor}
-        style={styles}
-      />
+      <div className="canvas">
+        <ReactSketchCanvas
+          ref={canvasRef}
+          height="600px"
+          width="600px"
+          strokeWidth={activeBrushSize}
+          strokeColor={activeColor}
+          style={styles}
+        />
+      </div>
       <div className="color-picker">
+        Color Picker
         {pallete.map((color) => {
           return (
             <div
               key={color}
-              className="dot"
+              className="color-dot"
               style={{ backgroundColor: color }}
               onClick={() => setActiveColor(color)}
             ></div>
           );
         })}
       </div>
+      <div className="color-picker">
+        Brush Size
+        <div
+          className="small-brush"
+          onClick={() => setActiveBrushSize(4)}
+        ></div>
+        <div
+          className="large-brush"
+          onClick={() => setActiveBrushSize(16)}
+        ></div>
+      </div>
+
+      <label>
+        <input
+          name="tool"
+          type="radio"
+          checked={activeTool == "pen"}
+          onChange={() => setActiveTool("pen")}
+        />
+        Pen
+      </label>
+      <label>
+        <input
+          name="tool"
+          type="radio"
+          checked={activeTool == "eraser"}
+          onChange={() => setActiveTool("eraser")}
+        />
+        Eraser
+      </label>
+
       <button onClick={handleSave}>Save</button>
       <style jsx>{`
         .editor {
+        }
+
+        .canvas {
+          cursor: url(${circleSVG}) 6 6, auto;
         }
 
         .color-picker {
@@ -71,9 +127,23 @@ const Editor = ({
           gap: 0.5rem;
         }
 
-        .dot {
+        .color-dot {
           width: 32px;
           height: 32px;
+          border-radius: 50%;
+        }
+
+        .small-brush {
+          width: 16px;
+          height: 16px;
+          background-color: ${activeColor};
+          border-radius: 50%;
+        }
+
+        .large-brush {
+          width: 32px;
+          height: 32px;
+          background-color: ${activeColor};
           border-radius: 50%;
         }
       `}</style>
