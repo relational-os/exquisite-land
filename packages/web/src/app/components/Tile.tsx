66fbac9ce51bcdf474b33a6cf2e43070c3304ffe
CJ Pais <cj@cjpais.com>
2021-09-13 15:21:56 -0700

    add colorization for user

--- packages/web/src/app/components/Tile.tsx
+++ packages/web/src/app/components/Tile.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import useStore from '../features/State';
-import { useFetchTile } from '@app/features/Graph';
-import PALETTES from 'src/constants/Palettes';
+import React from "react";
+import useStore from "../features/State";
+import { useFetchTile } from "@app/features/Graph";
+import PALETTES from "src/constants/Palettes";
+import { useWallet } from "@gimmixorg/use-wallet";
 
 const Tile = ({
   x,
   y,
   handleTileClick,
-  style
+  style,
 }: {
   x: number;
   y: number;
   handleTileClick: () => void;
   style?: React.CSSProperties;
 }) => {
-  console.log('Tile rendering', x, y);
-  const activeCanvasID = useStore(state => state.activeCanvas);
+  console.log("Tile rendering", x, y);
+
+  const { account } = useWallet();
+  const activeCanvasID = useStore((state) => state.activeCanvas);
   const palette = PALETTES[activeCanvasID];
   const { tile } = useFetchTile(activeCanvasID, x, y);
+
+  // ALTERNATE CODE FOR RENDERING OWNERSHIP OF TILE
+  // background-color: ${tile &&
+  // account &&
+  // tile.owner.id == account.toLowerCase()
+  //   ? "#00FF0066"
+  //   : "#FF000066"};
   return (
     <div className="tile" onClick={handleTileClick} style={style}>
       {tile?.svg && (
@@ -28,9 +38,14 @@ const Tile = ({
         .tile {
           position: relative;
           border: 1px dashed ${palette[2]}77;
+          background-color: ${tile
+            ? `#${tile.owner.id.slice(2, 8)}33`
+            : "#FFFFFF66"};
         }
         .tile:hover {
-          background-color: #f1f1f1;
+          background-color: ${account
+            ? `#${account.slice(2, 8)}33`
+            : "#f1f1f1"};
         }
         .svg {
           width: 100%;
