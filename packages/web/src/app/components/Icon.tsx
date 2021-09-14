97495b2955f38bd6ecaeefb7fdc916de6d40d032
Scott Dodge <scott@scotato.com>
2021-09-13 22:12:55 -0700

    update icons to use hand drawn svgs, add @svgr/webpack for svg importing

--- packages/web/src/app/components/Icon.tsx
+++ packages/web/src/app/components/Icon.tsx
@@ -1,43 +1,56 @@
-import Check from '@app/icons/Check';
-import Eraser from '@app/icons/Eraser';
-import PaintBrush from '@app/icons/PaintBrush';
-import Palette from '@app/icons/Palette';
-import Pencil from '@app/icons/Pencil';
-import Times from '@app/icons/Times';
+import { forwardRef } from "react";
+import PaintBrush from "@app/graphics/icon-brush-large.svg";
+import Pencil from "@app/graphics/icon-brush-small.svg";
+import Eraser from "@app/graphics/icon-eraser.svg";
+import Swatch1 from "@app/graphics/palette-blob-1.svg";
+import Swatch2 from "@app/graphics/palette-blob-2.svg";
+import Swatch3 from "@app/graphics/palette-blob-3.svg";
+import Swatch4 from "@app/graphics/palette-blob-4.svg";
+import Swatch5 from "@app/graphics/palette-blob-5.svg";
 
 interface IconProps {
   name: IconName;
   className?: string;
 }
 
-type IconName =
-  | 'check'
-  | 'eraser'
-  | 'paint-brush'
-  | 'palette'
-  | 'pencil'
-  | 'times';
+export type IconName =
+  | "eraser"
+  | "paint-brush"
+  | "pencil"
+  | "swatch-1"
+  | "swatch-2"
+  | "swatch-3"
+  | "swatch-4"
+  | "swatch-5";
 
-const Icon = (props: IconProps) => {
-  return <IconSwitch name={props.name} />;
-};
+const Icon = forwardRef(function Icon(
+  props: React.ComponentPropsWithoutRef<"svg"> & IconProps,
+  ref: React.Ref<HTMLOrSVGElement>
+) {
+  const Component = iconSwitch(props.name);
+  return <Component ref={ref} {...props} />;
+});
 
 export default Icon;
 
-const IconSwitch = ({ name }: { name: IconName }) => {
+const iconSwitch = (name: IconName) => {
   switch (name.toLowerCase()) {
-    case 'check':
-      return <Check />;
-    case 'eraser':
-      return <Eraser />;
-    case 'paint-brush':
-      return <PaintBrush />;
-    case 'palette':
-      return <Palette />;
-    case 'pencil':
-      return <Pencil />;
-    case 'times':
-      return <Times />;
+    case "eraser":
+      return Eraser;
+    case "paint-brush":
+      return PaintBrush;
+    case "pencil":
+      return Pencil;
+    case "swatch-1":
+      return Swatch1;
+    case "swatch-2":
+      return Swatch2;
+    case "swatch-3":
+      return Swatch3;
+    case "swatch-4":
+      return Swatch4;
+    case "swatch-5":
+      return Swatch5;
     default:
       throw new Error(`Invalid icon name: ${name}`);
   }
