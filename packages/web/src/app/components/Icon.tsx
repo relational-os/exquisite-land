c425a8872cfbc825ac515c6b76655757a002abce
Dave Gorum <dave@meaning.app>
2021-09-16 16:51:53 -0700

    add pixel icon definitions

--- packages/web/src/app/components/Icon.tsx
+++ packages/web/src/app/components/Icon.tsx
@@ -1,6 +1,8 @@
 import { forwardRef } from "react";
-import PaintBrush from "@app/graphics/icon-brush-large.svg";
-import Pencil from "@app/graphics/icon-brush-small.svg";
+import PaintBucket from "@app/graphics/px-icon-bucket.svg";
+import Pencil from "@app/graphics/px-icon-pencil.svg";
+import Eyedropper from "@app/graphics/px-icon-eyedropper.svg";
+import Undo from "@app/graphics/px-icon-undo.svg";
 import Eraser from "@app/graphics/icon-eraser.svg";
 import Swatch1 from "@app/graphics/palette-blob-1.svg";
 import Swatch2 from "@app/graphics/palette-blob-2.svg";
@@ -9,49 +11,55 @@ import Swatch4 from "@app/graphics/palette-blob-4.svg";
 import Swatch5 from "@app/graphics/palette-blob-5.svg";
 
 interface IconProps {
-  name: IconName;
-  className?: string;
+	name: IconName;
+	className?: string;
 }
 
 export type IconName =
-  | "eraser"
-  | "paint-brush"
-  | "pencil"
-  | "swatch-1"
-  | "swatch-2"
-  | "swatch-3"
-  | "swatch-4"
-  | "swatch-5";
+	| "undo"
+	| "eyedropper"
+	| "eraser"
+	| "paint-bucket"
+	| "pencil"
+	| "swatch-1"
+	| "swatch-2"
+	| "swatch-3"
+	| "swatch-4"
+	| "swatch-5";
 
 const Icon = forwardRef(function Icon(
-  props: React.ComponentPropsWithoutRef<"svg"> & IconProps,
-  ref: React.Ref<HTMLOrSVGElement>
+	props: React.ComponentPropsWithoutRef<"svg"> & IconProps,
+	ref: React.Ref<HTMLOrSVGElement>
 ) {
-  const Component = iconSwitch(props.name);
-  return <Component ref={ref} {...props} />;
+	const Component = iconSwitch(props.name);
+	return <Component ref={ref} {...props} />;
 });
 
 export default Icon;
 
 const iconSwitch = (name: IconName) => {
-  switch (name.toLowerCase()) {
-    case "eraser":
-      return Eraser;
-    case "paint-brush":
-      return PaintBrush;
-    case "pencil":
-      return Pencil;
-    case "swatch-1":
-      return Swatch1;
-    case "swatch-2":
-      return Swatch2;
-    case "swatch-3":
-      return Swatch3;
-    case "swatch-4":
-      return Swatch4;
-    case "swatch-5":
-      return Swatch5;
-    default:
-      throw new Error(`Invalid icon name: ${name}`);
-  }
+	switch (name.toLowerCase()) {
+		case "eyedropper":
+			return Eyedropper;
+		case "undo":
+			return Undo;
+		case "eraser":
+			return Eraser;
+		case "paint-bucket":
+			return PaintBucket;
+		case "pencil":
+			return Pencil;
+		case "swatch-1":
+			return Swatch1;
+		case "swatch-2":
+			return Swatch2;
+		case "swatch-3":
+			return Swatch3;
+		case "swatch-4":
+			return Swatch4;
+		case "swatch-5":
+			return Swatch5;
+		default:
+			throw new Error(`Invalid icon name: ${name}`);
+	}
 };
