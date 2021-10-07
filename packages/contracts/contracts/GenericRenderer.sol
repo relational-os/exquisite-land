f18263d5aa87aa55793e799b0546a1c20d55c398
CJ Pais <cj@cjpais.com>
2021-10-07 11:48:49 -0700

    better renderer with tests

--- packages/contracts/contracts/GenericRenderer.sol
+++ packages/contracts/contracts/GenericRenderer.sol
@@ -8,17 +8,20 @@ contract GenericRenderer {
 
   // prettier-ignore
   string[256] lookup = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120","121","122","123","124","125","126","127","128","129","130","131","132","133","134","135","136","137","138","139","140","141","142","143","144","145","146","147","148","149","150","151","152","153","154","155","156","157","158","159","160","161","162","163","164","165","166","167","168","169","170","171","172","173","174","175","176","177","178","179","180","181","182","183","184","185","186","187","188","189","190","191","192","193","194","195","196","197","198","199","200","201","202","203","204","205","206","207","208","209","210","211","212","213","214","215","216","217","218","219","220","221","222","223","224","225","226","227","228","229","230","231","232","233","234","235","236","237","238","239","240","241","242","243","244","245","246","247","248","249","250","251","252","253","254","255"];
+  // TODO: ^ is it more gas efficient to construct this on the fly using char codes directly?
+
   uint8 constant NUM_PIXELS_PER_BYTE = 2;
   uint16 constant MAX_COLORS = 16;
   uint8 constant MAX_ROWS = 64;
   uint8 constant MAX_COLS = 64;
   uint16 constant MAX_DATA_SIZE = 512;
   string constant SVG_OPENER =
-    '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 32 32">';
+    '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 -0.5 ';
+  string constant CLOSE_SVG_OPENER = '">';
   string constant SVG_CLOSER = '</svg>';
   string constant PATH_PREFIX = '<path stroke="';
   string constant PATH_START_DATA = '" d="';
-  string constant PATH_END_DATA = '"/>';
+  string constant END_TAG = '"/>';
 
   // TODO: it might be useful to try to have a 2d array here for scratch instead of using memory
 
@@ -49,7 +52,7 @@ contract GenericRenderer {
     uint8 num_cols
   ) public view returns (string memory) {}
 
-  function renderSVGGeneric(
+  function renderSVG(
     bytes calldata data,
     string[] calldata palette,
     uint16 num_colors,
@@ -106,57 +109,18 @@ contract GenericRenderer {
       }
     }
 
-    string memory output = SVG_OPENER;
+    string memory output = string(
+      abi.encodePacked(
+        SVG_OPENER,
+        lookup[num_cols],
+        ' ',
+        lookup[num_rows],
+        CLOSE_SVG_OPENER
+      )
+    );
     for (uint16 i = 0; i < num_colors; i++) {
       if (isEmptyString(paths[i])) continue;
-      output = string(abi.encodePacked(output, paths[i], PATH_END_DATA));
-    }
-
-    output = string(abi.encodePacked(output, SVG_CLOSER));
-
-    return output;
-  }
-
-  function renderSVG(bytes calldata data, string[16] calldata palette)
-    public
-    view
-    returns (string memory)
-  {
-    // prettier-ignore
-    string[256] memory paths = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
-
-    for (uint16 h = 0; h < MAX_ROWS; h++) {
-      for (uint16 m = 0; m < MAX_COLS; m) {
-        uint16 index = (h * MAX_ROWS) + m;
-        uint8 c = countConsecutive(data, index, 32);
-        uint8 ci = uint8(data[index]);
-
-        if (isEmptyString(paths[ci])) {
-          paths[ci] = string(
-            abi.encodePacked(PATH_PREFIX, palette[ci], PATH_START_DATA)
-          );
-        }
-
-        paths[ci] = string(
-          abi.encodePacked(
-            paths[ci],
-            'M',
-            lookup[m],
-            ' ',
-            lookup[h],
-            'h',
-            lookup[c]
-          )
-        );
-
-        m += c;
-      }
-    }
-
-    string memory output = SVG_OPENER;
-    for (uint16 i = 0; i < MAX_COLORS; i++) {
-      if (isEmptyString(paths[i])) continue;
-      output = string(abi.encodePacked(output, paths[i], PATH_END_DATA));
+      output = string(abi.encodePacked(output, paths[i], END_TAG));
     }
 
     output = string(abi.encodePacked(output, SVG_CLOSER));
---
+++ packages/contracts/test/renderer.ts
@@ -0,0 +1,248 @@
+import { expect } from 'chai';
+import { ethers, waffle } from 'hardhat';
+import fs from 'fs';
+
+import chroma from 'chroma-js';
+
+import GenericRendererArtifact from '../artifacts/contracts/GenericRenderer.sol/GenericRenderer.json';
+import { GenericRenderer } from '../typechain';
+
+const { deployContract } = waffle;
+
+const RAINBOW_SCALE = chroma
+  .scale(['#f00', '#0f0', '#00f', '#f00'])
+  .mode('hsl');
+const CUBEHELIX_SCALE = chroma.cubehelix().gamma(0.6).scale();
+const XQST_LAND_PALETTE = [
+  '#e4a672',
+  '#b86f50',
+  '#743f39',
+  '#3f2832',
+  '#9e2835',
+  '#e53b44',
+  '#fb922b',
+  '#ffe762',
+  '#63c64d',
+  '#327345',
+  '#193d3f',
+  '#4f6781',
+  '#afbfd2',
+  '#ffffff',
+  '#2ce8f4',
+  '#0484d1'
+];
+
+function saveSVG(data: string, name: string) {
+  // mkdir test/svg
+  fs.mkdir('test/svg', (e) => {
+    if (e && e.code != 'EEXIST') {
+      console.log(e);
+    }
+  });
+  fs.writeFile(`test/svg/${name}.svg`, data, (e) => {
+    if (e) console.log(e);
+    else console.log(`wrote test/svg/${name}.svg`);
+  });
+}
+
+async function renderCubeHelix(
+  renderer: GenericRenderer,
+  numRows: number,
+  numCols: number,
+  numColors: number
+) {
+  const result = await renderer.renderSVG(
+    generatePixels(numRows, numCols, numColors),
+    CUBEHELIX_SCALE.colors(numColors),
+    numColors,
+    numRows,
+    numCols
+  );
+
+  saveSVG(result, `HELIX_${numColors}COLORS_${numCols}x${numRows}`);
+}
+
+async function renderRainbow(
+  renderer: GenericRenderer,
+  numRows: number,
+  numCols: number,
+  numColors: number
+) {
+  const result = await renderer.renderSVG(
+    generatePixels(numRows, numCols, numColors),
+    RAINBOW_SCALE.colors(numColors),
+    numColors,
+    numRows,
+    numCols
+  );
+
+  saveSVG(result, `RAINBOW_${numColors}COLORS_${numCols}x${numRows}`);
+}
+
+function generatePixels(nRows: number, nCols: number, nColors: number) {
+  var s = '0x';
+  for (var i = 0; i < nRows * nCols; i++) {
+    let colorIndex = i % nColors;
+    if ((colorIndex % nColors) / 16 < 1) {
+      s += '0' + colorIndex.toString(16);
+    } else {
+      s += colorIndex.toString(16);
+    }
+  }
+  return s;
+}
+
+describe('Renderer', () => {
+  let renderer: GenericRenderer;
+
+  beforeEach(async () => {
+    // 1
+    const signers = await ethers.getSigners();
+
+    // 2
+    renderer = (await deployContract(
+      signers[0],
+      GenericRendererArtifact
+    )) as GenericRenderer;
+  });
+
+  describe('32x32 - 16 Colors', () => {
+    it('Should render 32x32 with 16 Colors', async function () {
+      const WIDTH = 32;
+      const HEIGHT = 32;
+      const NUM_COLORS = 16;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('32x32 - 32 Colors', function () {
+    it('Should render 32x32 with 32 Colors', async function () {
+      const WIDTH = 32;
+      const HEIGHT = 32;
+      const NUM_COLORS = 32;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('32x32 - 256 Colors', function () {
+    it('Should render 32x32 with 256 Colors', async function () {
+      const WIDTH = 32;
+      const HEIGHT = 32;
+      const NUM_COLORS = 256;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('48x48 - 48 Colors', function () {
+    it('Should render 48x48 with 48 Colors', async function () {
+      const WIDTH = 48;
+      const HEIGHT = 48;
+      const NUM_COLORS = 48;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('48x48 - 16 Colors', function () {
+    it('Should render 48x48 with 16 Colors', async function () {
+      const WIDTH = 48;
+      const HEIGHT = 48;
+      const NUM_COLORS = 16;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('54x54 - 54 Colors', function () {
+    it('Should render 54x54 with 54 Colors', async function () {
+      const WIDTH = 54;
+      const HEIGHT = 54;
+      const NUM_COLORS = 54;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('54x54 - 16 Colors', function () {
+    it('Should render 54x54 with 16 Colors', async function () {
+      const WIDTH = 54;
+      const HEIGHT = 54;
+      const NUM_COLORS = 16;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('32x32 - 2 Colors', function () {
+    it('Should render 32x32 with 2 Colors', async function () {
+      const WIDTH = 32;
+      const HEIGHT = 32;
+      const NUM_COLORS = 2;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('56x56 - 56 Colors', function () {
+    it('Should render 56x56 with 56 Colors', async function () {
+      const WIDTH = 56;
+      const HEIGHT = 56;
+      const NUM_COLORS = 56;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('60x60 - 60 Colors', function () {
+    it('Should render 60x60 with 60 Colors', async function () {
+      const WIDTH = 60;
+      const HEIGHT = 60;
+      const NUM_COLORS = 60;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('60x60 - 60 Colors Cube', function () {
+    it('Should render 60x60 with 60 Colors', async function () {
+      const WIDTH = 60;
+      const HEIGHT = 60;
+      const NUM_COLORS = 60;
+
+      const done = await renderCubeHelix(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('56x56 - 16 Colors', function () {
+    it('Should render 56x56 with 16 Colors', async function () {
+      const WIDTH = 56;
+      const HEIGHT = 56;
+      const NUM_COLORS = 16;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('64x64 - 16 Colors', function () {
+    it('Should render 64x64 with 16 Colors', async function () {
+      const WIDTH = 64;
+      const HEIGHT = 64;
+      const NUM_COLORS = 16;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+
+  describe('64x64 - 64 Colors', function () {
+    it('Should render 64x64 with 64 Colors', async function () {
+      const WIDTH = 64;
+      const HEIGHT = 64;
+      const NUM_COLORS = 64;
+
+      const done = await renderRainbow(renderer, HEIGHT, WIDTH, NUM_COLORS);
+    });
+  });
+});
