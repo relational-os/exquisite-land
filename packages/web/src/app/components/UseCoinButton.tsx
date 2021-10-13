cd0dcee611094d6353935519b75c6e44870c76ab
Kevin Ingersoll <kingersoll@gmail.com>
2021-10-13 10:35:28 -0700

    Improve coin dropzone UX (#20)
    * optimize images
    
    * move coin drop handler into a hook so we can use parts of it elsewhere
    
    * wire up document-wide drop target
    
    * preload coinbox images

--- packages/web/src/app/components/UseCoinButton.tsx
+++ packages/web/src/app/components/UseCoinButton.tsx
@@ -1,19 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import CoinDropModal from '@app/components/modals/CoinDropModal';
+import Head from 'next/head';
+
+const listen = (
+  target: EventTarget,
+  ...args: Parameters<typeof target.addEventListener>
+) => {
+  target.addEventListener(...args);
+  return () => target.removeEventListener(...args);
+};
 
 const UseCoinButton = () => {
   const [isCoinModalOpen, setIsCoinModalOpen] = useState(false);
   const openCoinModal = () => setIsCoinModalOpen(true);
   const closeCoinModal = () => setIsCoinModalOpen(false);
 
+  // Allow dragging files into window/document to open modal so you don't need to click the coin button first. This is somewhat brittle in that the conditionals and `useEffect` dependencies need to ~stay where they're at for this to work properly. This also doesn't check for the right drop (i.e. a single PNG image file) but I thought it felt better to show the drop modal and later error that the wrong files were dropped rather than giving no feedback to the user.
+  const [hasDrop, setHasDrop] = useState(false);
+  useEffect(() => {
+    const removeDragEnter = listen(document, 'dragenter', (event) => {
+      // Do nothing if document is not focused (i.e. tabbed out)
+      if (!document.hasFocus()) return;
+      setHasDrop(true);
+    });
+    const removeDragLeave = listen(document, 'dragleave', (event) => {
+      if (hasDrop) {
+        setHasDrop(false);
+      }
+    });
+    const removeVisibilityChange = listen(document, 'visibilitychange', () => {
+      if (document.hidden) {
+        setHasDrop(false);
+      }
+    });
+    const removeBlur = listen(window, 'blur', () => {
+      setHasDrop(false);
+    });
+
+    return () => {
+      removeDragEnter();
+      removeDragLeave();
+      removeVisibilityChange();
+      removeBlur();
+    };
+  }, []);
+
   return (
     <div className="use-coin-button">
+      <Head>
+        {/* Preload coin images */}
+        <link
+          rel="preload"
+          href="/graphics/coinbox-empty.png"
+          as="image"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          href="/graphics/coinbox-arrow.png"
+          as="image"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          href="/graphics/coinbox-background.png"
+          as="image"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          href="/graphics/coinbox-valid.png"
+          as="image"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          href="/graphics/coinbox-invalid.png"
+          as="image"
+          type="image/png"
+        />
+      </Head>
+
       <button className="invite-button" onClick={openCoinModal}>
         have a coin?
       </button>
+
       <Modal
-        isOpen={isCoinModalOpen}
+        isOpen={isCoinModalOpen || hasDrop}
         onRequestClose={closeCoinModal}
         style={modalStyles}
       >
