19fd230b29b16aca343cd8a662569561d79d5796
CJ Pais <cj@cjpais.com>
2021-10-21 15:38:13 -0700

    create simple library for the renderer

--- packages/web/src/app/features/TileUtils.ts
+++ packages/web/src/app/features/TileUtils.ts
@@ -7,9 +7,36 @@ export const getCoordinates = (tokenId: number): [number, number] => [
   tokenId & 0x000000ff
 ];
 
+const PATH_SVG_OPENER =
+  '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 -0.5 ';
+const CLOSE_SVG_OPENER = '">';
+const PATH_PREFIX = '<path stroke="';
+const PATH_START_DATA = '" d="';
+const END_TAG = '"/>';
+
 const SVG_OPENER =
   '<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 32 32">';
 const SVG_CLOSER = '</svg>';
+
+function transpose(matrix: any) {
+  return matrix.reduce(
+    (prev: any, next: any) =>
+      next.map((_: any, i: number) => (prev[i] || []).concat(next[i])),
+    []
+  );
+}
+
+export const getEthPixelData = (pixels: Pixels): string => {
+  let transposed = transpose(pixels);
+  let flattened = transposed.flat();
+  let outputPixels = '0x';
+  for (let i = 0; i < flattened.length; i++) {
+    let d = `${flattened[i].toString(16).padStart(2, '0')}`;
+    outputPixels += d;
+  }
+
+  return outputPixels;
+};
 
 export const getSVGFromPixels = (pixels: Pixels) => {
   var output = SVG_OPENER;
@@ -24,3 +51,53 @@ export const getSVGFromPixels = (pixels: Pixels) => {
   output += SVG_CLOSER;
   return output;
 };
+
+export const getSVGFromData = (
+  data: string,
+  palette: string[],
+  numRows: number,
+  numCols: number
+) => {
+  const dataBytes = hexToBytes(data);
+  let numColors = palette.length;
+  let paths = new Array(numColors).fill('');
+
+  for (let h = 0; h < numRows; h++) {
+    for (let m = 0; m < numCols; m) {
+      let index = h * numCols + m;
+
+      let c = countConsecutive(index, numCols, dataBytes);
+      let ci = dataBytes[index];
+
+      paths[ci] += `M${m} ${h}h${c}`;
+      m += c;
+    }
+  }
+
+  let output = `${PATH_SVG_OPENER}${numCols} ${numRows}${CLOSE_SVG_OPENER}`;
+
+  for (let i = 0; i < numColors; i++) {
+    if (paths[i] == '') continue;
+    output += `${PATH_PREFIX}${palette[i]}${PATH_START_DATA}${paths[i]}${END_TAG}`;
+  }
+
+  output += SVG_CLOSER;
+  return output;
+};
+
+function hexToBytes(hex: string): number[] {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+function countConsecutive(index: number, numCols: number, data: number[]) {
+  var num = 1;
+
+  while ((index + num) % numCols != 0) {
+    if (data[index] == data[index + num]) num++;
+    else break;
+  }
+
+  return num;
+}
