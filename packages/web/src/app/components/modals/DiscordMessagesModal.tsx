6aebc3a23ef3b22d25048239944d03acc68c3898
CJ Pais <cj@cjpais.com>
2021-10-25 14:36:02 -0700

    clean up the display a bit

--- packages/web/src/app/components/modals/DiscordMessagesModal.tsx
+++ packages/web/src/app/components/modals/DiscordMessagesModal.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 
 const DiscordMessagesModal = () => {
@@ -10,24 +11,32 @@ const DiscordMessagesModal = () => {
   }, []);
 
   return (
-    <div className="transaction-history-modal">
+    <div className="discord-messages-modal">
       {messages.map((m) => {
         return (
           <div className="message">
-            <img
-              src={`https://cdn.discordapp.com/avatars/${m.author.id}/${m.author.avatar}.png`}
-            />
-            <div>{m.author.username}</div>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <div>{m.author.username}</div>
+              <img
+                height="64px"
+                width="64px"
+                src={`https://cdn.discordapp.com/avatars/${m.author.id}/${m.author.avatar}.png`}
+              />
+              <div>{dayjs(m.timestamp).format('MMM d, h:mma')}</div>
+            </div>
             <div>{m.content}</div>
-            <div>{m.timestamp}</div>
           </div>
         );
       })}
       <style jsx>{`
         .discord-messages-modal {
+          font-size: 1.25rem;
+          color: white;
         }
         .message {
           display: flex;
+          align-items: center;
+          padding-bottom: 1rem;
         }
       `}</style>
     </div>
