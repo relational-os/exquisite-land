c5fbea151ffd43dd791fc1ba4882d3cc161fd8af
CJ Pais <cj@cjpais.com>
2021-11-09 15:05:04 -0800

    bump renderer to 64 colors at 56x56

--- packages/contracts/contracts/GenericRenderer.sol
+++ packages/contracts/contracts/GenericRenderer.sol
@@ -7,11 +7,10 @@ contract GenericRenderer {
   // prettier-ignore
   string[256] lookup = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100','101','102','103','104','105','106','107','108','109','110','111','112','113','114','115','116','117','118','119','120','121','122','123','124','125','126','127','128','129','130','131','132','133','134','135','136','137','138','139','140','141','142','143','144','145','146','147','148','149','150','151','152','153','154','155','156','157','158','159','160','161','162','163','164','165','166','167','168','169','170','171','172','173','174','175','176','177','178','179','180','181','182','183','184','185','186','187','188','189','190','191','192','193','194','195','196','197','198','199','200','201','202','203','204','205','206','207','208','209','210','211','212','213','214','215','216','217','218','219','220','221','222','223','224','225','226','227','228','229','230','231','232','233','234','235','236','237','238','239','240','241','242','243','244','245','246','247','248','249','250','251','252','253','254','255'];
 
-  uint8 constant NUM_PIXELS_PER_BYTE = 2; // TODO implement data compression
-
-  uint16 constant MAX_COLORS = 16;
+  uint16 constant MAX_COLORS = 64;
   uint8 constant MAX_ROWS = 56;
   uint8 constant MAX_COLS = 56;
+  uint8 constant MAX_MULTIPLIER = 16;
 
   string constant SVG_OPENER =
     '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 -0.5 ';
@@ -30,14 +29,14 @@ contract GenericRenderer {
     uint16 numColors,
     uint16 numRows,
     uint16 numCols
-  ) internal view returns (bytes[256] memory, uint8[16] memory) {
+  ) internal view returns (bytes[] memory, uint8[] memory) {
     uint256 startGas = gasleft();
     uint8 c;
-    bytes[256] memory paths;
+    bytes[] memory paths = new bytes[](MAX_COLORS * MAX_MULTIPLIER);
 
     // bytes[] memory paths = new bytes[](numColors);
     // prettier-ignore
-    uint8[16] memory multiplier = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    uint8[] memory multiplier = new uint8[](MAX_COLORS);
 
     for (uint16 h = 0; h < numRows; h++) {
       for (uint16 m = 0; m < numCols; m) {
@@ -52,15 +51,15 @@ contract GenericRenderer {
         }
         /* == END: Get RLE length ==*/
 
-        uint8 ci = uint8(data[index]);
+        uint16 ci = uint16(uint8(data[index]));
 
         // Optimize gas by not creating long strings
-        ci = uint8(ci + (numColors * multiplier[ci]));
+        ci = uint16(ci + (numColors * multiplier[ci]));
         if (paths[ci].length > 700) {
-          if (multiplier[ci % numColors] < 16) {
+          if (multiplier[ci % numColors] < MAX_MULTIPLIER) {
             multiplier[ci % numColors]++;
           }
-          ci = uint8(
+          ci = uint16(
             uint8(data[index]) + numColors * multiplier[ci % numColors]
           );
         }
@@ -106,7 +105,7 @@ contract GenericRenderer {
 
     uint256 startGas = gasleft();
 
-    (bytes[256] memory paths, uint8[16] memory multiplier) = getPaths(
+    (bytes[] memory paths, uint8[] memory multiplier) = getPaths(
       data,
       numColors,
       numRows,
