e81642e017ba35a21bf81de0f542f4372d972582
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-11-16 19:22:26 -0600

    Clean up migrations and schema

--- packages/discord/prisma/migrations/20211112183336_/migration.sql
+++ packages/discord/prisma/migrations/20211112183336_/migration.sql
@@ -4,11 +4,23 @@ CREATE TABLE "User" (
     "discordId" TEXT NOT NULL,
     "discordUsername" TEXT NOT NULL,
     "discordDiscriminator" TEXT NOT NULL,
+    "discordAvatar" TEXT NOT NULL,
     "address" TEXT,
+    "roles" TEXT[],
+    "lastChecked" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "joined" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Tile" (
+    "id" TEXT NOT NULL,
+    "discordSent" BOOLEAN NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User.discordId_unique" ON "User"("discordId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tile.id_unique" ON "Tile"("id");
