c2fef82b77cb8e060c2986c158481ed3e34c7568
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-02 13:21:45 -0600

    Add more logging

--- packages/discord/src/pages/api/invite.ts
+++ packages/discord/src/pages/api/invite.ts
@@ -9,6 +9,8 @@ const api: NextApiHandler = async (req, res) => {
   const { signature, account }: { signature: string; account: string } =
     req.body;
   const signingAccount = verifyMessage(SIGNING_MESSAGE, signature);
+  console.log({ signature });
+  console.log({ account });
 
   if (account.toLowerCase() != signingAccount.toLowerCase())
     return res.status(400).json({ error: 'Invalid signature' });
