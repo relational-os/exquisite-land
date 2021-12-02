fb837025bcb41e3a3ff5913aa15d9e31c30d7ae1
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-02 13:44:00 -0600

    Add POST request check to skip preflight

--- packages/discord/src/pages/api/invite.ts
+++ packages/discord/src/pages/api/invite.ts
@@ -6,32 +6,34 @@ const SIGNING_MESSAGE =
   'I HEREBY INVITE THE GORBLIN IN AND ASSUME ALL RESPONSIBILITY FOR ANY SLIMINGS';
 
 const api: NextApiHandler = async (req, res) => {
-  const { signature, account }: { signature: string; account: string } =
-    req.body;
-  const signingAccount = verifyMessage(SIGNING_MESSAGE, signature);
-  console.log({ signature });
-  console.log({ account });
+  if (req.method == 'POST') {
+    const { signature, account }: { signature: string; account: string } =
+      req.body;
+    const signingAccount = verifyMessage(SIGNING_MESSAGE, signature);
+    console.log({ signature });
+    console.log({ account });
 
-  if (account.toLowerCase() != signingAccount.toLowerCase())
-    return res.status(400).json({ error: 'Invalid signature' });
+    if (account.toLowerCase() != signingAccount.toLowerCase())
+      return res.status(400).json({ error: 'Invalid signature' });
 
-  console.log(`signature matches! signed by ${account}`);
+    console.log(`signature matches! signed by ${account}`);
 
-  let introMessage = `sup I'ma coin y'alls asses. Thanks for the invite, ${account}`;
+    let introMessage = `sup I'ma coin y'alls asses. Thanks for the invite, ${account}`;
 
-  fetch(`${process.env.HOST}/api/message`, {
-    method: 'POST',
-    body: JSON.stringify({ content: introMessage }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => res.json());
+    fetch(`${process.env.HOST}/api/message`, {
+      method: 'POST',
+      body: JSON.stringify({ content: introMessage }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => res.json());
 
-  await prisma.gorblinInviter.create({
-    data: { address: account }
-  });
+    await prisma.gorblinInviter.create({
+      data: { address: account }
+    });
 
-  return res.json({ success: true });
+    return res.json({ success: true });
+  }
 };
 
 export default api;
