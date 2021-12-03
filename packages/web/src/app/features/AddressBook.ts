1a8a2711b31526f125d8cf722a55e201d85f36c7
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-02 21:03:55 -0600

    Add webhook env file link

--- packages/web/src/app/features/AddressBook.ts
+++ packages/web/src/app/features/AddressBook.ts
@@ -21,3 +21,22 @@ export const FORWARDER_CONTRACT_ADDRESS = isProd
 export const GRAPH_URL = isProd
   ? 'https://api.thegraph.com/subgraphs/name/relational-os/exquisite-land'
   : 'https://api.thegraph.com/subgraphs/name/relational-os/exquisite-land-canary';
+
+export const DISCORD_CHANNELS = {
+  'terra-masu': '888518144346427392',
+  'bot-testing': '910957967849967619',
+  'admin-chat': '888520558969487391'
+};
+
+export const DISCORD_WEBHOOKS = {
+  gorblin: {
+    'admin-chat': process.env.GORBLIN_ADMIN_CHAT,
+    'bot-testing': process.env.GORBLIN_BOT_TESTING,
+    'terra-masu': process.env.GORBLIN_TERRA_MASU
+  },
+  xqst: {
+    'admin-chat': process.env.XQST_ADMIN_CHAT,
+    'bot-testing': process.env.XQST_BOT_TESTING,
+    'terra-masu': process.env.XQST_TERRA_MASU
+  }
+};
