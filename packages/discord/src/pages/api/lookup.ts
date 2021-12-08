1dd18fa0e4cdd29499382878edb54ce048e4143a
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-08 10:01:08 -0600

    Add lookup endpoint for Discord ID

---
+++ packages/discord/src/pages/api/lookup.ts
@@ -0,0 +1,28 @@
+import prisma from '@server/helpers/prisma';
+import { NextApiHandler } from 'next';
+import NextCors from 'nextjs-cors';
+
+const api: NextApiHandler = async (req, res) => {
+  console.log(req.query);
+  //   await NextCors(req, res, {
+  //     methods: ['GET'],
+  //     origin: '*',
+  //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  //   });
+
+  const user = await prisma.user.findFirst({
+    where: { address: req.query.address as string },
+    select: {
+      discordId: true,
+      address: true
+    }
+  });
+  if (!user) return res.status(404).end();
+
+  return res.json({
+    address: user.address,
+    id: user.discordId
+  });
+};
+
+export default api;
